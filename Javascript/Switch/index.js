64058743f0278239d93486668fcc608abc519ae8
mauriciogregory <mauricio.gre@gmail.com>
2022-03-14 16:49:21 -0300

    upload

--- Javascript/Switch/index.js
+++ Javascript/Switch/index.js
@@ -1,4 +1,4 @@
-var month = 1;
+var month = 13;
 
 switch (month) {
   case 1:
@@ -8,4 +8,10 @@ switch (month) {
   case 2:
     document.write("Fevereiro");
     break;
+
+  case 3:
+      document.write("Março");
+      break;
+  default: 
+      document.write("Mês não encontrado.")
 }
